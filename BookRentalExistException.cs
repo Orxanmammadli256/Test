aa8ed410faf8401b48ec9f5399d365701e83d802
Orxanmammadli256 <mammadliorxan050@gmail.com>
2024-12-15 14:28:18 +0400

    a

---
+++ BookRentalExistException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Business.Exceptions
+{
+    public class BookRentalExistException : Exception
+    {
+        public BookRentalExistException(string message) : base(message)
+        {
+            
+        }
+    }
+}
